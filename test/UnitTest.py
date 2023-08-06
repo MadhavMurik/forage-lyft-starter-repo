2732b1e2b0f331f124fed82f419ffb122564dba0
Madhav Murikipudi <madhavmurikipudi@madhavs-mbp.lan>
2023-08-06 18:25:27 -0400

    testing version

--- test/UnitTest.py
+++ test/UnitTest.py
@@ -1,11 +1,8 @@
 import unittest
 from datetime import datetime
 
-from engine.model.calliope import Calliope
-from engine.model.glissade import Glissade
-from engine.model.palindrome import Palindrome
-from engine.model.rorschach import Rorschach
-from engine.model.thovex import Thovex
+from carFactory import Calliope, Glissade,Palindrome,Rorschach,Thovex
+
 
 
 class TestCalliope(unittest.TestCase):
